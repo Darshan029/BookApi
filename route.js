273e00cf2682c03efd302bd5bda9ac75725b65da
Darshan029 <dvaghasiya048@rku.ac.in>
2021-02-02 10:44:02 +0530

    user register login add

--- route.js
+++ route.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const Book = require('./Model/book');
 const router = express.Router();
+//for user
+const user = require('./Model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //fetch record
 router.get("/books",async (req,res)=>{
@@ -48,4 +52,42 @@ router.delete("/books/:id",async(req,res)=>{
     }
     
 });
+
+//user
+
+router.post('/register',async (req,res)=>{
+
+    const salt = await bcrypt.genSalt(10);
+    const hashePswd = await bcrypt.hash(req.body.pswd,salt);
+
+    const user = new User({
+        uname:req.body.uname,
+        pswd:hashePswd
+    })
+
+    await user.save();
+    res.send(user);
+
+});
+
+router.post("/login",async (req,res)=>{
+
+    const user = user.findOne({uname:req.body.uname});
+    if(!user){
+        return res.send("user not found..!");
+    }
+    else{
+        const isValid = bcrypt.compare(req.body.pswd,user.pswd);
+        if(!isValid){
+            res.send("Wrong Password..!");
+        }
+        else{
+            //res.send("Login Successfully..!");
+            const token = jwt.sign({_id:user._id},"privatekey");
+            res.header('auth-token',token);
+            res.send(token);
+        }
+    }
+});
+
 module.exports = router;
